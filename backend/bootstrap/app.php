a3dd170a18428ed1f96b888279b5a998d4bf7d69
Sébastien Etievant <etievant.sebastien@protonmail.com>
2026-03-19 15:35:17 +0100

    feat: intégrer Sentry (Laravel + React) pour le monitoring des erreurs en prod

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -18,5 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->report(function (Throwable $e): void {
+            if (app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
+        });
     })->create();
